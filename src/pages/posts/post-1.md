31840643fc036e835dcae749e92b7992196fa3c1
Matt <director@acidgambit.com>
2024-02-02 18:30:46 -0500

    maj updates

---
+++ src/pages/posts/post-1.md
@@ -0,0 +1,33 @@
+---
+layout: "@/layouts/PostLayout.astro"
+title: "My First Blog Post"
+pubDate: 2022-07-01
+description: "This is the first post of my new Astro blog."
+author: "Acid Gambit"
+image:
+  url: "https://docs.astro.build/assets/full-logo-light.png"
+  alt: "The full Astro logo."
+tags: ["astro", "blogging", "learning in public"]
+---
+
+Welcome to my _new blog_ about learning Astro! Here, I will share my learning journey as I build a new website.
+
+# Hello
+
+## What I've accomplished
+
+### three hashes
+
+1. **Installing Astro**: First, I created a new Astro project and set up my online accounts.
+
+2. **Making Pages**: I then learned how to make pages by creating new `.astro` files and placing them in the `src/pages/` folder.
+
+3. **Making Blog Posts**: This is my first blog post! I now have Astro pages and Markdown posts!
+
+## What's next
+
+I will finish the Astro tutorial, and then keep adding more posts. Watch this space for more to come.
+
+We all have moments when the absurdity of existence seems to abruptly dawn upon us. For those with an absurdist bent of mind, such glimpses can become prolonged reflections - a certain lens through which everyday experiences are filtered and found. I do not claim to understand reality in its totality nor assert these ephemeral, absurd musings as objective truth. They represent but one perspective among many, born in quiet instants of introspection.
+
+One place it often hits me is on the subway. My mind starts wandering as I stare off into space while being jostled in the crowded train car. I begin contemplating the utter strangeness and pointlessness of what we're all doing - cramming ourselves into metal boxes so we can rush somewhere and do some task, only to do it again the next day.
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,62 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Geist Sans"],
+        mono: ["Geist Mono"],
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        nav: "hsl(var(--background-opacity))",
+        foreground: "hsl(var(--foreground))",
+
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [],
+};
